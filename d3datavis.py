6243168c0bbb38b50f0568afb77e84233d323af9
C Hamilton <publisher@solarviews.com>
2016-11-30 13:59:53 -0500

    removed the need for resources.py

--- d3datavis.py
+++ d3datavis.py
@@ -3,9 +3,6 @@ from PyQt4.QtGui import *
 from qgis.core import *
 from qgis.gui import *
 
-# Initialize Qt resources from file resources.py
-import resources
-
 import os.path
 from heatmapDialog import HeatmapDialog
 
@@ -18,7 +15,7 @@ class D3DataVis:
     def initGui(self):
         """ Initialize the menu and dialog boxes for the D3 heatmap chart """
         self.heatmapDialog = HeatmapDialog(self.iface, self.iface.mainWindow())
-        icon = QIcon(":/plugins/d3datavis/icon.png")
+        icon = QIcon(os.path.dirname(__file__) + "/icon.png")
         self.heatmapAction = QAction(icon, u"Circular Date/Time Heatmap", self.iface.mainWindow())
         self.heatmapAction.triggered.connect(self.showHeatmapDialog)
         self.heatmapAction.setCheckable(False)
