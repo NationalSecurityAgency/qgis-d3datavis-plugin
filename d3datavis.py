56de2a9c9d9466bb250c12b79f5398132923981d
C Hamilton <publisher@solarviews.com>
2018-02-06 10:17:49 -0500

    fast loading speed improvements

--- d3datavis.py
+++ d3datavis.py
@@ -2,17 +2,16 @@ from qgis.PyQt.QtGui import QIcon
 from qgis.PyQt.QtWidgets import QAction
 
 import os.path
-from .heatmapDialog import HeatmapDialog
 
 
 
 class D3DataVis:
+    heatmapDialog = None
     def __init__(self, iface):
         self.iface = iface
 
     def initGui(self):
         """ Initialize the menu and dialog boxes for the D3 heatmap chart """
-        self.heatmapDialog = HeatmapDialog(self.iface, self.iface.mainWindow())
         icon = QIcon(os.path.dirname(__file__) + "/icon.png")
         self.heatmapAction = QAction(icon, "Circular Date/Time Heatmap", self.iface.mainWindow())
         self.heatmapAction.triggered.connect(self.showHeatmapDialog)
@@ -27,6 +26,9 @@ class D3DataVis:
     
     def showHeatmapDialog(self):
         """Display the circular date/time heatmap dialog box"""
+        if not self.heatmapDialog:
+            from .heatmapDialog import HeatmapDialog
+            self.heatmapDialog = HeatmapDialog(self.iface, self.iface.mainWindow())
         self.heatmapDialog.show()
         
         
