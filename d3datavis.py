b562aee4ed77e64d59c5b2ae7757f43c3388d4d2
C Hamilton <hamiltoncj@users.noreply.github.com>
2023-05-19 12:54:09 +0000

    Add help menu

--- d3datavis.py
+++ d3datavis.py
@@ -1,3 +1,4 @@
+from qgis.PyQt.QtCore import QUrl
 from qgis.PyQt.QtGui import QIcon
 from qgis.PyQt.QtWidgets import QAction
 
@@ -19,18 +20,24 @@ class D3DataVis:
 
     def initGui(self):
         """ Initialize the menu and dialog boxes for the D3 heatmap chart """
-        icon = QIcon(os.path.dirname(__file__) + "/icon.png")
+        icon = QIcon(os.path.dirname(__file__) + "/icons/icon.png")
         self.heatmapAction = QAction(icon, "Circular Date/Time Heatmap", self.iface.mainWindow())
         self.heatmapAction.triggered.connect(self.showHeatmapDialog)
         self.iface.addPluginToWebMenu("D3 Data Visualization", self.heatmapAction)
         self.iface.addWebToolBarIcon(self.heatmapAction)
         
         if has_wordcloud:
-            icon = QIcon(os.path.dirname(__file__) + "/wordcloud.png")
+            icon = QIcon(os.path.dirname(__file__) + "/icons/wordcloud.png")
             self.wordCloudAction = QAction(icon, "Generate Word Cloud", self.iface.mainWindow())
             self.wordCloudAction.triggered.connect(self.showWordCloudDialog)
             self.iface.addPluginToWebMenu("D3 Data Visualization", self.wordCloudAction)
             self.iface.addWebToolBarIcon(self.wordCloudAction)
+
+        # Help
+        icon = QIcon(os.path.dirname(__file__) + '/icons/help.svg')
+        self.helpAction = QAction(icon, "Help", self.iface.mainWindow())
+        self.helpAction.triggered.connect(self.help)
+        self.iface.addPluginToWebMenu('D3 Data Visualization', self.helpAction)
 
     def unload(self):
         self.iface.removePluginWebMenu("D3 Data Visualization", self.heatmapAction)
@@ -38,6 +45,7 @@ class D3DataVis:
         if has_wordcloud:
             self.iface.removePluginWebMenu("D3 Data Visualization", self.wordCloudAction)
             self.iface.removeWebToolBarIcon(self.wordCloudAction)
+        self.iface.removePluginWebMenu('D3 Data Visualization', self.helpAction)
     
     def showHeatmapDialog(self):
         """Display the circular date/time heatmap dialog box"""
@@ -51,5 +59,9 @@ class D3DataVis:
             from .genwordcloud import WordCloudDialog
             self.wordcloudDialog = WordCloudDialog(self.iface, self.iface.mainWindow())
         self.wordcloudDialog.show()
-        
-        
+
+    def help(self):
+        '''Display a help page'''
+        import webbrowser
+        url = QUrl.fromLocalFile(os.path.dirname(__file__) + "/index.html").toString()
+        webbrowser.open(url, new=2)
