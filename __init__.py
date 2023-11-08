e5b8fa0fab32fb4f6b374d7252560b5d0750c052
C Hamilton <hamiltoncj@users.noreply.github.com>
2023-11-08 14:37:02 +0000

    Add GPL license to each file.

--- __init__.py
+++ __init__.py
@@ -1,3 +1,13 @@
+"""
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
 def classFactory(iface):
     from .d3datavis import D3DataVis
     return D3DataVis(iface)
